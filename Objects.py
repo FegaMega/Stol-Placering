64275e32923a1057a9f2fcf533d8e0b83f806d1f
FegaMega <david.smidebrant@gmail.com>
2025-03-11 19:58:53 +0100

    shit

--- Objects.py
+++ Objects.py
@@ -2,5 +2,9 @@ import pygame
 
 class Table:
    def __init__(self, type:str, pos, size, children=[], scale=1):
-        self.type = type
-        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[0])
+      self.type = type
+      self.rect = pygame.Rect(pos[0], pos[1], size[0], size[0])
+   def draw(self, screen: pygame.display):
+      match self.type:
+         case "Round":
+            surface = pygame.import
