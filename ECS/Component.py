4dbf22f49a6d99da3a88ac168a336a69de878b69
FegaMega <david.smidebrant@gmail.com>
2025-03-15 14:46:06 +0100

    asdf

--- ECS/Component.py
+++ ECS/Component.py
@@ -1,72 +1,107 @@
 import pygame
 import math
 
+def getTextEvent(text:str, Event):
+   for event in Event:
+      if event.type == pygame.KEYDOWN:
+         if event.key == pygame.K_RETURN:
+            return 0
+         elif event.key == pygame.K_BACKSPACE:
+            text = text[:-1]
+         else:
+            text += event.unicode
+   return text
+
 class Entity:
    def __init__(self):
       self.Component = {}
 
 
 class TransformComponent:
-   def __init__(self, rect):
+   def __init__(self, rect, Master=True):
       self.rect = rect
+      self.Master = Master
    
+class SpriteComponent:
+   def __init__(self, color, radius=10, outlineColor=(0, 0, 0), outlineWidth=0):
+      self.outline = {
+         "width" : outlineWidth,
+         "radius" : radius,
+         "color" : outlineColor
+      }
+      self.body = {
+         "color" : color,
+         "radius" : radius
+      }
+
+
 class TextComponent:
    def __init__(self, Text:str, Color: tuple, Font:pygame.font.Font):
       self.Text = Text
       self.Color = Color
       self.Font = Font
+      self.textReleave = 10
       self.Sprite = self.Font.render(self.Text, True, self.Color)
       self.rect = self.Sprite.get_rect()
 
 
 class Manager:
    def __init__(self):
-      self.Components = []
       self.Entitys = []
 
    def newComponent(self, EntID: int, type, *args):
       
       T = type(*args)
-
-      self.Components.append(T)
       
-      self.Entitys[EntID].Component[str(type)] = self.Components.index(T)
+      self.Entitys[EntID].Component[str(type)] = T
 
    def getComponent(self, EntID, type):
-      id = self.Entitys[EntID].Component[str(type)]
-      return self.Components[id]
+      return self.Entitys[EntID].Component[str(type)]
    
    def hasComponent(self, EntID, type):
       return str(type) in self.Entitys[EntID].Component
    
    def newEntity(self):
+
       ent = Entity()
-      for slot in self.Entitys:
-         if slot == None:
-            slot = ent
-         return self.Entitys.index(ent)
       
+      for slot in self.Entitys:
+      
+         if slot == None:
+      
+            slot = ent
+      
+            return self.Entitys.index(ent)
+
       self.Entitys.append(ent)
             
-      self.Entitys.index(ent)
+      return self.Entitys.index(ent)
 
    def delEntity(self, EntID):
       self.Entitys[EntID] = None
       return 0
 
 
-def draw(manager, Ent, screen):
+def draw(manager: Manager, Ent, screen):
 
    if not manager.hasComponent(Ent, TransformComponent):
       manager.newComponent(Ent, TransformComponent, pygame.Rect(100, 100, 50, 50))
 
    transform = manager.getComponent(Ent, TransformComponent)
+
+   if not manager.hasComponent(Ent, SpriteComponent):
+      manager.newComponent(Ent, SpriteComponent, (0, 0, 0))
+
+   sprite = manager.getComponent(Ent, SpriteComponent)
+
    
-   surface = pygame.Surface(transform.rect.size)
-   
+   surface = pygame.Surface(transform.rect.size).convert_alpha()
+   surface.fill((0, 0, 0, 0))   
    rect = surface.get_rect()
 
-   pygame.draw.rect(surface, (255, 255, 255), rect)
+   pygame.draw.rect(surface, sprite.body["color"], rect, border_radius=sprite.body["radius"])
+
+   pygame.draw.rect(surface, sprite.outline["color"], rect, sprite.outline["width"], sprite.outline["radius"])
    
    if manager.hasComponent(Ent, TextComponent):
 
@@ -76,3 +111,30 @@ def draw(manager, Ent, screen):
 
    screen.blit(surface, transform.rect)
 
+def changeName(manager: Manager, Ent, Event):
+
+   if not manager.hasComponent(Ent, TextComponent):
+      return -1
+   
+   textComp:TextComponent  = manager.getComponent(Ent, TextComponent)
+   
+   text = getTextEvent(textComp.Text, Event)
+
+   if text == 0:
+      return 1
+
+   textComp.Text = text
+
+   textComp.Sprite = textComp.Font.render(textComp.Text, True, textComp.Color)
+   textComp.rect = textComp.Sprite.get_rect()
+
+   if not manager.hasComponent(Ent, TransformComponent):
+      manager.newComponent(Ent, TransformComponent, textComp.rect)
+   
+   transform : TransformComponent= manager.getComponent(Ent, TransformComponent)
+   if transform.Master:
+      transform.rect.width = max(transform.rect.width, textComp.rect.width + 2*textComp.textReleave)
+   else:
+      transform.rect.width = textComp.rect.width + 2*textComp.textReleave
+
+   return 0
--- ECS/ECSTest.py
+++ ECS/ECSTest.py
@@ -6,22 +6,130 @@ pygame.init()
 
 font = pygame.font.SysFont("Helvetica", 20)
 
+def mouseCollision(A, B:pygame.Rect):
+    return (A[0] >= B.x and A[0] <= B.x + B.width) and (A[1] >= B.y and A[1] <= B.y + B.height)
 
-Objects = {
-   "People" : [],
-   "Tables" : []
-}
+class app:
+   def __init__(self):
+      self.Scene = self.getScene()
+      self.mouseHolding = [None, ""]
+      self.mouseSelected = None
+      self.cursorIMG = pygame.SYSTEM_CURSOR_ARROW
+      self.screen = pygame.display.set_mode((700, 700))
+      self.Event = pygame.event.get()
+      self.running = True
 
-screen = pygame.display.set_mode((700, 700))
-r = True
-while r:
-   for event in pygame.event.get():
+   def getHover(self) -> list:
+      mousePos = pygame.mouse.get_pos()
+      #Moves backwards so the top most person get picked first
+      for x in range(len(self.Scene["People"])-1, -1, -1):
+         
+         transform = manager.getComponent(self.Scene["People"][x], TransformComponent)
+
+         if mouseCollision(mousePos, transform.rect): return [self.Scene["People"][x], "Normal"]
+
+   # #Moves backwards so the top most table get picked first   
+   #  for x in range(len(Scene["Tables"])-1, -1, -1):
+   #
+   #     result, flag = Scene["Tables"][x].getMouseTouching(mousePos)
+   #
+   #     if result:
+   #
+   #         return Scene["Tables"][x], flag 
+      return [None, ""]
+   
+   def getHolding(self) -> object:
+
+      hover = self.getHover()
+      
+      #Nice Cursor change for ease of use 
+      if hover[0] != None:
+
+         if hover[1] == "Edge":
+            self.cursorIMG = pygame.SYSTEM_CURSOR_SIZENWSE
+
+         else:
+            self.cursorIMG = pygame.SYSTEM_CURSOR_SIZEALL      
+      
+      #Left mouse button
+      if not pygame.mouse.get_pressed()[0]:
+         return None, ""
+      
+      #If already holding something
+      if self.mouseHolding[0] != None:
+
+         if self.mouseHolding[1] == "Edge":
+            self.cursorIMG = pygame.SYSTEM_CURSOR_SIZENWSE
+         else:
+            self.cursorIMG = pygame.SYSTEM_CURSOR_SIZEALL      
+
+         return self.mouseHolding
+
+      return hover
+
+
+   def getSelected(self) -> object:
+
+      #Deselects if left mouse button is pressed
+      if pygame.mouse.get_pressed()[0]:
+         return None
+      
+      #If something already selected
+      if self.mouseSelected != None:
+         return self.mouseSelected
+
+      #Select if right mouse button is pressed
+      if pygame.mouse.get_pressed()[2]:
+         return self.getHover()[0]
+
+
+
+   def getScene(self):
+      #Placeholder function
+      
+      scene = {
+         "People" : [],
+         "Tables" : []
+      }
+      for i in range (0, 5):   
+         t = manager.newEntity()
+         rect = pygame.Rect(100*i, 100, 50,  50)
+         manager.newComponent(t, TransformComponent, rect, False)
+         manager.newComponent(t, SpriteComponent, (255, 255, 255), 10, (0, 0, 0), 2)
+         manager.newComponent(t, TextComponent, "", (0, 0, 0), font)
+         scene["People"].append(t)
+
+
+      return scene
+
+
+   
+App = app()
+while App.running:
+   App.Event = pygame.event.get()
+   for event in App.Event:
       if event.type == pygame.QUIT:
-         r = False
+         App.running = False
+
+   pygame.mouse.set_cursor(App.cursorIMG)
+   App.cursorIMG = pygame.SYSTEM_CURSOR_ARROW
+   
+   App.screen.fill((255, 255, 255))
 
    #draw
-   screen.fill((0, 0, 0))
+   for Person in App.Scene["People"]:
+      draw(manager, Person, App.screen)
 
-      
+   App.mouseHolding = App.getHolding()
+   if App.mouseHolding[0] != None:
+      Hover = manager.getComponent(App.mouseHolding[0], TransformComponent)
+      Hover.rect.center = pygame.mouse.get_pos()
+
+   App.mouseSelected = App.getSelected()
+
+   if App.mouseSelected != None:
+      r = changeName(manager, App.mouseSelected, App.Event)
+      if r == 1:
+         App.mouseSelected == None
    pygame.display.update()
    pygame.time.Clock().tick(60)
